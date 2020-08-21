fae51d2a95df6efbf23d8e94449dd91928c20cc7
Alex NGOMBOL <gpetitalex10@gmail.com>
2020-08-21 10:38:11 +0100

    Improve contact section

--- resources/views/partials/home/contact.blade.php
+++ resources/views/partials/home/contact.blade.php
@@ -3,31 +3,28 @@
         <div class="row">
 
             <div class="ml-auto col-lg-5 col-md-6 col-12">
-                <h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200">Feel free to ask anything</h2>
+                <h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200">Envoyez nous un message</h2>
 
                 <form action="#" method="post" class="contact-form webform" data-aos="fade-up" data-aos-delay="400" role="form">
-                    <input type="text" class="form-control" name="cf-name" placeholder="Name">
+                    <input type="text" class="form-control" name="cf-name" placeholder="Nom" id="cf-name">
 
-                    <input type="email" class="form-control" name="cf-email" placeholder="Email">
+                    <input type="email" class="form-control" name="cf-email" placeholder="Email" id="cf-email">
 
-                    <textarea class="form-control" rows="5" name="cf-message" placeholder="Message"></textarea>
+                    <textarea class="form-control" rows="5" name="cf-message" placeholder="Message" id="cf-message"></textarea>
 
-                    <button type="submit" class="form-control" id="submit-button" name="submit">Send Message</button>
+                    <button type="submit" class="form-control" id="submit-button" name="submit">Envoyer</button>
                 </form>
             </div>
 
             <div class="mx-auto mt-4 mt-lg-0 mt-md-0 col-lg-5 col-md-6 col-12">
-                <h2 class="mb-4" data-aos="fade-up" data-aos-delay="600">Where you can <span>find us</span></h2>
+                <h2 class="mb-4" data-aos="fade-up" data-aos-delay="600">Où nous retrouver</h2>
 
-                <p data-aos="fade-up" data-aos-delay="800"><i class="fa fa-map-marker mr-1"></i> 120-240 Rio de Janeiro - State of Rio de Janeiro, Brazil</p>
-                <!-- How to change your own map point
-                    1. Go to Google Maps
-                    2. Click on your location point
-                    3. Click "Share" and choose "Embed map" tab
-                    4. Copy only URL and paste it within the src="" field below
-                -->
+                <p data-aos="fade-up" data-aos-delay="800">
+                    <i class="fa fa-map-marker mr-1"></i>Bonapriso Douala, CAMEROUN
+                </p>
+
                 <div class="google-map" data-aos="fade-up" data-aos-delay="900">
-                    <iframe src="https://maps.google.com/maps?q=Av.+Lúcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="1920" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    <iframe width="1920" height="250"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.975159429634!2d9.69788616525108!3d4.025480148261639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x106112c7426691ff%3A0x310e860af24cda5!2sBonapriso%2C%20Douala!5e0!3m2!1sfr!2scm!4v1597665163772!5m2!1sfr!2scm" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
 
