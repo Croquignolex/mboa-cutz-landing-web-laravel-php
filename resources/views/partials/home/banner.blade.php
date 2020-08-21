656eebd3dee44ba502888994b6ad38f6b5d3dd5c
Alex NGOMBOL <gpetitalex10@gmail.com>
2020-08-21 09:16:09 +0100

    Improve banner

--- resources/views/partials/home/banner.blade.php
+++ resources/views/partials/home/banner.blade.php
@@ -8,13 +8,13 @@
             <div class="col-lg-8 col-md-10 mx-auto col-12">
                 <div class="hero-text mt-5 text-center">
 
-                    <h6 data-aos="fade-up" data-aos-delay="300">new way to build a healthy lifestyle!</h6>
+                    <h6 data-aos="fade-up" data-aos-delay="300">Une nouvelle unique de vous coiffer</h6>
 
-                    <h1 class="text-white" data-aos="fade-up" data-aos-delay="500">Upgrade your body at Gymso Fitness</h1>
+                    <h1 class="text-white" data-aos="fade-up" data-aos-delay="500">Faites confiant à l'expert de tout les cheveux</h1>
 
-                    <a href="#feature" class="btn custom-btn mt-3" data-aos="fade-up" data-aos-delay="600">Get started</a>
+{{--                    <a href="#description" class="btn custom-btn mt-3" data-aos="fade-up" data-aos-delay="600">Description</a>--}}
 
-                    <a href="#about" class="btn custom-btn bordered mt-3" data-aos="fade-up" data-aos-delay="700">learn more</a>
+                    <a href="#about" class="btn custom-btn bordered mt-3" data-aos="fade-up" data-aos-delay="700">En savoir plus</a>
 
                 </div>
             </div>
@@ -24,40 +24,42 @@
 </section>
 
 
-<section class="feature" id="feature">
+<section class="feature" id="description">
     <div class="container">
         <div class="row">
 
             <div class="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
-                <h2 class="mb-3 text-white" data-aos="fade-up">New to the gymso?</h2>
+                <h2 class="mb-3 text-white" data-aos="fade-up">Chez MBOA CUTZ</h2>
 
-                <h6 class="mb-4 text-white" data-aos="fade-up">Your membership is up to 2 months FREE ($62.50 per month)</h6>
+                <h6 class="mb-4 text-white" data-aos="fade-up">Des offres exclusives vous attends</h6>
 
-                <p data-aos="fade-up" data-aos-delay="200">Gymso is free HTML template by <a rel="nofollow" href="https://www.tooplate.com" target="_parent">Tooplate</a> for your commercial website. Bootstrap v4.2.1 Layout. Feel free to use it.</p>
+                <p data-aos="fade-up" data-aos-delay="200" class="text-justify">
+                    Nous vous assurons un service de qualité et des produits validé par les experts.
+                    N'ésitez surtout pas a consulter le catalogue de nos différents produits et services.
+                </p>
 
-                <a href="#" class="btn custom-btn bg-color mt-3" data-aos="fade-up" data-aos-delay="300" data-toggle="modal" data-target="#membershipForm">Become a member today</a>
+                <a href="javascript: void(0);" class="btn custom-btn bg-color mt-3" data-aos="fade-up" data-aos-delay="300">Découvrez nos produits</a>
             </div>
 
             <div class="mr-lg-auto mt-3 col-lg-4 col-md-6 col-12">
                 <div class="about-working-hours">
                     <div>
 
-                        <h2 class="mb-4 text-white" data-aos="fade-up" data-aos-delay="500">Working hours</h2>
+                        <h2 class="mb-4 text-white" data-aos="fade-up" data-aos-delay="500">Horaires</h2>
 
-                        <strong class="d-block" data-aos="fade-up" data-aos-delay="600">Sunday : Closed</strong>
+                        <strong class="d-block" data-aos="fade-up" data-aos-delay="600">Lundi : <span class="text-danger">Fermé</span></strong>
 
-                        <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Monday - Friday</strong>
+                        <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Mardi - Samedi</strong>
 
-                        <p data-aos="fade-up" data-aos-delay="800">7:00 AM - 10:00 PM</p>
+                        <p data-aos="fade-up" data-aos-delay="800">7:00 - 22:00</p>
 
-                        <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Saturday</strong>
+                        <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Dimanche</strong>
 
-                        <p data-aos="fade-up" data-aos-delay="800">6:00 AM - 4:00 PM</p>
+                        <p data-aos="fade-up" data-aos-delay="800">8:00 - 16:00</p>
                     </div>
                 </div>
             </div>
         </div>
 
-    </div>
     </div>
 </section>
